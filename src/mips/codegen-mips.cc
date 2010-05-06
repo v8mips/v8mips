904bf3e7af8f1b604e5daddf6838fd7c1b0b1048
Alexandre Rames <alexandre.rames@gmail.com>
2010-05-06 17:43:50 +0200

    [Fix] Fix 2 floating points comparison.

--- src/mips/codegen-mips.cc
+++ src/mips/codegen-mips.cc
@@ -4834,7 +4834,7 @@ static void EmitTwoNonNanDoubleComparison(MacroAssembler* masm, Condition cc) {
   __ nop();
 
   // Not equal, not less, not NaN, must be greater.
-  __ li(v0, Operand(EQUAL));
+  __ li(v0, Operand(GREATER));
   __ Ret();
 
   __ bind(&equal);
