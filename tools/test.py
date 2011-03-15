391e99f9e31e3edcae20c37a6314a88bbe1ce5e2
Svetozar Janjic <Svetozar.Janjic@rt-rk.com>
2011-03-15 12:03:10 -0700

    [MIPS] Add option --nocrankshaft-only to test.py to run tests once.
    Run tests with --nocrankshaft option.

--- tools/test.py
+++ tools/test.py
@@ -1229,6 +1229,9 @@ def BuildOptions():
       dest="store_unexpected_output", action="store_false")
   result.add_option("--xmlout", help="File name of the UnitTest output")
   result.add_option("--xmltestsuite", help="Name of the testsuite in the UnitTest XML output", default="v8tests")
+  result.add_option("--nocrankshaft-only",
+                    help="Only run tests with nocrankshaft version",
+                    default=False, action="store_true")
   result.add_option("--stress-only",
                     help="Only run tests with --always-opt --stress-opt",
                     default=False, action="store_true")
@@ -1281,6 +1284,8 @@ def ProcessOptions(options):
   global XMLOUT
   XMLOUT = options.xmlout
   global VARIANT_FLAGS
+  if options.nocrankshaft_only:
+    VARIANT_FLAGS = [['--nocrankshaft']]
   if options.stress_only:
     VARIANT_FLAGS = [['--stress-opt', '--always-opt']]
   if options.nostress:
