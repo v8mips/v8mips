a2140508d9ea0c0fa60be002b88f395c89b60b48
Daniel Kalmar <kalmard@homejinni.com>
2011-03-21 16:44:40 +0000

    [MIPS merge] Small cleanup of bug ids
    Ported commits:
    b0d5bdc4baaa6731f2befa42652686f9048bd403

--- src/mips/assembler-mips.cc
+++ src/mips/assembler-mips.cc
@@ -242,7 +242,6 @@ const Instr kLwSwOffsetMask = kImm16Mask;
 Assembler::Assembler(void* buffer, int buffer_size)
     : positions_recorder_(this),
       allow_peephole_optimization_(false) {
-  // BUG(3245989): disable peephole optimization if crankshaft is enabled.
   allow_peephole_optimization_ = FLAG_peephole_optimization;
   if (buffer == NULL) {
     // Do our own buffer management.
