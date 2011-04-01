0f33ef89a4299e9c190dbf95d0e8ca5b6f8c4b6d
Paul Lind <plind@mips.com>
2011-03-31 18:02:06 -0700

    [MIPS] Transcendental stub requires input in a0.
    This stub requires input param in a0 also, which full-codegen does
    not do by default.

--- src/mips/full-codegen-mips.cc
+++ src/mips/full-codegen-mips.cc
@@ -2929,6 +2929,7 @@ void FullCodeGenerator::EmitMathSin(ZoneList<Expression*>* args) {
                                TranscendentalCacheStub::TAGGED);
   ASSERT(args->length() == 1);
   VisitForStackValue(args->at(0));
+  __ mov(a0, result_register());  // Stub requires parameter in a0 and on tos.
   __ CallStub(&stub);
   context()->Plug(v0);
 }
@@ -2940,6 +2941,7 @@ void FullCodeGenerator::EmitMathCos(ZoneList<Expression*>* args) {
                                TranscendentalCacheStub::TAGGED);
   ASSERT(args->length() == 1);
   VisitForStackValue(args->at(0));
+  __ mov(a0, result_register());  // Stub requires parameter in a0 and on tos.
   __ CallStub(&stub);
   context()->Plug(v0);
 }
@@ -2951,6 +2953,7 @@ void FullCodeGenerator::EmitMathLog(ZoneList<Expression*>* args) {
                                TranscendentalCacheStub::TAGGED);
   ASSERT(args->length() == 1);
   VisitForStackValue(args->at(0));
+  __ mov(a0, result_register());  // Stub requires parameter in a0 and on tos.
   __ CallStub(&stub);
   context()->Plug(v0);
 }
