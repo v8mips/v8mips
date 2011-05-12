9f87a5a5d79682d27e98eed84175ccfdf05ac674
Daniel Kalmar <kalmard@homejinni.com>
2011-05-12 10:50:56 +0000

    [MIPS] Temporarily change the default test variant to nocrankshaft-only in test.py. This is a workaround to a bug in test.py and should be reverted once that is fixed.

--- tools/test.py
+++ tools/test.py
@@ -622,9 +622,7 @@ def CarCdr(path):
 
 # Use this to run several variants of the tests, e.g.:
 # VARIANT_FLAGS = [[], ['--always_compact', '--noflush_code']]
-VARIANT_FLAGS = [[],
-                 ['--stress-opt', '--always-opt'],
-                 ['--nocrankshaft']]
+VARIANT_FLAGS = [['--nocrankshaft']]
 
 
 class TestConfiguration(object):
