91647167dabf96317dcb47121fec8ae9efe20ade
Paul Lind <plind@mips.com>
2011-05-24 17:48:41 -0700

    [MIPS] Style change.

--- src/mips/assembler-mips-inl.h
+++ src/mips/assembler-mips-inl.h
@@ -129,7 +129,7 @@ Object** RelocInfo::target_object_address() {
   // which can be de-referenced during heap iteration.
   ASSERT(IsCodeTarget(rmode_) || rmode_ == EMBEDDED_OBJECT);
   reconstructed_obj_ptr_ =
-    reinterpret_cast<Object*>(Assembler::target_address_at(pc_));
+      reinterpret_cast<Object*>(Assembler::target_address_at(pc_));
   return &reconstructed_obj_ptr_;
 }
 
