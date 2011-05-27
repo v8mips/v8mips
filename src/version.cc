21c2ecd5bed90e8a2a518f6768a536c6b54fd360
Douglas Leung <douglas@douglas-ub904.(none)>
2011-05-27 10:17:25 -0700

    Revert "Set version number to 3.2.3.1 (candidate)."
    This reverts commit fc50f1a1a6a27f02a491bcf8dcf9d463012ceee8.

--- src/version.cc
+++ src/version.cc
@@ -35,7 +35,7 @@
 #define MAJOR_VERSION     3
 #define MINOR_VERSION     2
 #define BUILD_NUMBER      3
-#define PATCH_LEVEL       1
+#define PATCH_LEVEL       0
 #define CANDIDATE_VERSION true
 
 // Define SONAME to have the SCons build the put a specific SONAME into the
