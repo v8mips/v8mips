8bc9e8d49a878647241a7bca3b5edcff71f1aff6
Daniel Kalmar <kalmard@homejinni.com>
2011-06-10 10:13:39 +0000

    [MIPS crankshaft merge] A collection of context-related refactoring changes.
    Ported commit:
    ef01bf0339c40a454b702245b456404643dabc08

--- src/mips/lithium-codegen-mips.cc
+++ src/mips/lithium-codegen-mips.cc
@@ -187,7 +187,7 @@ bool LCodeGen::GeneratePrologue() {
       FastNewContextStub stub(heap_slots);
       __ CallStub(&stub);
     } else {
-      __ CallRuntime(Runtime::kNewContext, 1);
+      __ CallRuntime(Runtime::kNewFunctionContext, 1);
     }
     RecordSafepoint(Safepoint::kNoDeoptimizationIndex);
     // Context is returned in both v0 and cp.  It replaces the context
