v8mips/v8mips

commit 0e05ccad0f881bc0c7c4c63042d8fb5bae42d7b9

Provide fast-path for CHECK macro.
Makes debug build 20% faster.

R=erik.corry@gmail.com

Review URL: http://codereview.chromium.org/8256011

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@9767 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/checks.h
@@ -63,7 +63,9 @@ static inline void CheckHelper(const char* file,
 
 // The CHECK macro checks that the given condition is true; if not, it
 // prints a message to stderr and aborts.
-#define CHECK(condition) CheckHelper(__FILE__, __LINE__, #condition, condition)
+#define CHECK(condition) do {                                             \
+    if (!(condition)) CheckHelper(__FILE__, __LINE__, #condition, false); \
+  } while(0)
 
 
 // Helper function used by the CHECK_EQ function when given int

commit 9643db01fe4cf8a9c85faf6b4fc973bd5d32dc61

Fix presubmit error introduced by r9767.
TBR=erik.corry@gmail.com

Review URL: http://codereview.chromium.org/8383031

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@9770 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/checks.h
@@ -65,7 +65,7 @@ static inline void CheckHelper(const char* file,
 // prints a message to stderr and aborts.
 #define CHECK(condition) do {                                             \
     if (!(condition)) CheckHelper(__FILE__, __LINE__, #condition, false); \
-  } while(0)
+  } while (0)
 
 
 // Helper function used by the CHECK_EQ function when given int