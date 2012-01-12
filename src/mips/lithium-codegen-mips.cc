a4b7508a1d3551afaa02b5ed1ddf4cc24a5e2e89
Paul Lind <plind@mips.com>
2012-01-11 19:23:08 -0800

    Revert "[MIPS crankshaft] Added a temporary assertion to DoGlobalObject."
    This reverts commit 932240af017c93af3ff3e73e82e2a41af601f60e.
    
    This was a debug assertion. Raytrace bench is now fixed, and the
    mjsunit array-sort bug seems to be unrelated to this.

--- src/mips/lithium-codegen-mips.cc
+++ src/mips/lithium-codegen-mips.cc
@@ -2697,8 +2697,6 @@ void LCodeGen::DoGlobalObject(LGlobalObject* instr) {
   Register context = ToRegister(instr->context());
   Register result = ToRegister(instr->result());
   __ lw(result, ContextOperand(cp, Context::GLOBAL_INDEX));
-  // TODO (kalmard): remove this once the bug is fixed
-  __ Assert(ne, "got zap value", result, Operand((int)kFromSpaceZapValue));
 }
 
 
