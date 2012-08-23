126b8e906b25bd9e723415399f045bf9ede6d3cc
Akos Palfi <palfia@homejinni.com>
2012-08-23 21:23:19 +0200

    MIPS: Allow uint32 value on optimized frames if they are consumed by safe operations.
    Port r12367 (27f16c18)
    
    Original commit message:
    Safe operations are those that either do not observe unsignedness or have special support for uint32 values:
    
    - all binary bitwise operations: they perform ToInt32 on inputs;
    - >> and << shifts: they perform ToInt32 on left hand side and ToUint32 on right hand side;
    - >>> shift: it performs ToUint32 on both inputs;
    - stores to integer external arrays (not pixel, float or double ones): these stores are "bitwise";
    - HChange: special support added for conversions of uint32 values to double and tagged values;
    - HSimulate: special support added for deoptimization with uint32 values in registers and stack slots;
    - HPhi: phis that have only safe uses and only uint32 operands are uint32 themselves.
    
    BUG=v8:2097
    TEST=test/mjsunit/compiler/uint32.js

--- src/mips/lithium-mips.h
+++ src/mips/lithium-mips.h
@@ -108,6 +108,7 @@ class LCodeGen;
   V(InstanceOfKnownGlobal)                      \
   V(InstructionGap)                             \
   V(Integer32ToDouble)                          \
+  V(Uint32ToDouble)                             \
   V(InvokeFunction)                             \
   V(IsConstructCallAndBranch)                   \
   V(IsNilAndBranch)                             \
@@ -136,6 +137,7 @@ class LCodeGen;
   V(MulI)                                       \
   V(NumberTagD)                                 \
   V(NumberTagI)                                 \
+  V(NumberTagU)                                 \
   V(NumberUntagD)                               \
   V(ObjectLiteral)                              \
   V(OsrEntry)                                   \
@@ -1571,6 +1573,16 @@ class LInteger32ToDouble: public LTemplateInstruction<1, 1, 0> {
 };
 
 
+class LUint32ToDouble: public LTemplateInstruction<1, 1, 0> {
+ public:
+  explicit LUint32ToDouble(LOperand* value) {
+    inputs_[0] = value;
+  }
+
+  DECLARE_CONCRETE_INSTRUCTION(Uint32ToDouble, "uint32-to-double")
+};
+
+
 class LNumberTagI: public LTemplateInstruction<1, 1, 0> {
  public:
   explicit LNumberTagI(LOperand* value) {
@@ -1578,6 +1590,16 @@ class LNumberTagI: public LTemplateInstruction<1, 1, 0> {
   }
 
   DECLARE_CONCRETE_INSTRUCTION(NumberTagI, "number-tag-i")
+};
+
+
+class LNumberTagU: public LTemplateInstruction<1, 1, 0> {
+ public:
+  explicit LNumberTagU(LOperand* value) {
+    inputs_[0] = value;
+  }
+
+  DECLARE_CONCRETE_INSTRUCTION(NumberTagU, "number-tag-u")
 };
 
 
