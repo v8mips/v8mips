v8mips/v8mips

commit 6c91c11b1dea88e18db1ca75b9f49d5bc5d3b2aa

Adding a fast path for parsing index keys.
Reduces overhead on http://code.google.com/p/chromium/issues/detail?id=156379 from 360ms down to 255ms.

Review URL: https://chromiumcodereview.appspot.com/11189039

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@12761 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/json-parser.h
@@ -301,23 +301,49 @@ Handle<Object> JsonParser<seq_ascii>::ParseJsonObject() {
   if (c0_ != '}') {
     do {
       if (c0_ != '"') return ReportUnexpectedCharacter();
-      Handle<String> key = ParseJsonSymbol();
-      if (key.is_null() || c0_ != ':') return ReportUnexpectedCharacter();
-      AdvanceSkipWhitespace();
-      Handle<Object> value = ParseJsonValue();
-      if (value.is_null()) return ReportUnexpectedCharacter();
 
-      uint32_t index;
-      if (key->AsArrayIndex(&index)) {
+      int start_position = position_;
+      Advance();
+
+      uint32_t index = 0;
+      while (c0_ >= '0' && c0_ <= '9') {
+        int d = c0_ - '0';
+        if (index > 429496729U - ((d > 5) ? 1 : 0)) break;
+        index = (index * 10) + d;
+        Advance();
+      }
+
+      if (position_ != start_position + 1 && c0_ == '"') {
+        AdvanceSkipWhitespace();
+
+        if (c0_ != ':') return ReportUnexpectedCharacter();
+        AdvanceSkipWhitespace();
+        Handle<Object> value = ParseJsonValue();
+        if (value.is_null()) return ReportUnexpectedCharacter();
+
         JSObject::SetOwnElement(json_object, index, value, kNonStrictMode);
-      } else if (key->Equals(isolate()->heap()->Proto_symbol())) {
-        prototype = value;
       } else {
-        if (JSObject::TryTransitionToField(json_object, key)) {
-          json_object->FastPropertyAtPut(current_index++, *value);
+        position_ = start_position;
+#ifdef DEBUG
+        c0_ = '"';
+#endif
+
+        Handle<String> key = ParseJsonSymbol();
+        if (key.is_null() || c0_ != ':') return ReportUnexpectedCharacter();
+
+        AdvanceSkipWhitespace();
+        Handle<Object> value = ParseJsonValue();
+        if (value.is_null()) return ReportUnexpectedCharacter();
+
+        if (key->Equals(isolate()->heap()->Proto_symbol())) {
+          prototype = value;
         } else {
-          JSObject::SetLocalPropertyIgnoreAttributes(
-              json_object, key, value, NONE);
+          if (JSObject::TryTransitionToField(json_object, key)) {
+            json_object->FastPropertyAtPut(current_index++, *value);
+          } else {
+            JSObject::SetLocalPropertyIgnoreAttributes(
+                json_object, key, value, NONE);
+          }
         }
       }
     } while (MatchSkipWhiteSpace(','));
@@ -374,14 +400,12 @@ Handle<Object> JsonParser<seq_ascii>::ParseJsonNumber() {
     if ('0' <= c0_ && c0_ <= '9') return ReportUnexpectedCharacter();
   } else {
     int i = 0;
-    int digits = 0;
     if (c0_ < '1' || c0_ > '9') return ReportUnexpectedCharacter();
     do {
       i = i * 10 + c0_ - '0';
-      digits++;
       Advance();
-    } while (c0_ >= '0' && c0_ <= '9');
-    if (c0_ != '.' && c0_ != 'e' && c0_ != 'E' && digits < 10) {
+    } while (c0_ >= '0' && c0_ <= '9' && i <= (kMaxInt - 9) / 10);
+    if (c0_ != '.' && c0_ != 'e' && c0_ != 'E') {
       SkipWhitespace();
       return Handle<Smi>(Smi::FromInt((negative ? -i : i)), isolate());
     }

commit 467922e0f930589c9da84aa6be418786268bc759

Fixed error introduced in r12761.
BUG=2373

Review URL: https://chromiumcodereview.appspot.com/11198068

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@12765 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/json-parser.h
@@ -400,12 +400,14 @@ Handle<Object> JsonParser<seq_ascii>::ParseJsonNumber() {
     if ('0' <= c0_ && c0_ <= '9') return ReportUnexpectedCharacter();
   } else {
     int i = 0;
+    int digits = 0;
     if (c0_ < '1' || c0_ > '9') return ReportUnexpectedCharacter();
     do {
       i = i * 10 + c0_ - '0';
+      digits++;
       Advance();
-    } while (c0_ >= '0' && c0_ <= '9' && i <= (kMaxInt - 9) / 10);
-    if (c0_ != '.' && c0_ != 'e' && c0_ != 'E') {
+    } while (c0_ >= '0' && c0_ <= '9');
+    if (c0_ != '.' && c0_ != 'e' && c0_ != 'E' && digits < 10) {
       SkipWhitespace();
       return Handle<Smi>(Smi::FromInt((negative ? -i : i)), isolate());
     }

test/mjsunit/regress/regress-2373.js
@@ -1,0 +1,29 @@
+// Copyright 2012 the V8 project authors. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//     * Neither the name of Google Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+var o = JSON.parse('{"a":2600753951}');
+assertEquals(2600753951, o.a);