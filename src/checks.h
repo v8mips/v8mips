v8mips/v8mips

commit 05b05424997d185041ff914b16725319dbef56c3

Use C++11 static_assert() if available.
R=svenpanne@chromium.org

Review URL: https://codereview.chromium.org/19684009

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@15736 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/checks.h
@@ -230,6 +230,13 @@ inline void CheckNonEqualsHelper(const char* file,
 #define CHECK_LE(a, b) CHECK((a) <= (b))
 
 
+// Use C++11 static_assert if possible, which gives error
+// messages that are easier to understand on first sight.
+#if __cplusplus >= 201103L || \
+    (defined(__has_feature) && __has_feature(cxx_static_assert)) || \
+    (defined(__has_extension) && __has_extension(cxx_static_assert))
+#define STATIC_CHECK(test) static_assert(test, #test)
+#else
 // This is inspired by the static assertion facility in boost.  This
 // is pretty magical.  If it causes you trouble on a platform you may
 // find a fix in the boost code.
@@ -249,6 +256,7 @@ template <int> class StaticAssertionHelper { };
   typedef                                                                     \
     StaticAssertionHelper<sizeof(StaticAssertion<static_cast<bool>((test))>)> \
     SEMI_STATIC_JOIN(__StaticAssertTypedef__, __LINE__)
+#endif
 
 
 extern bool FLAG_enable_slow_asserts;

commit 071799e757e79d8dd14c169d36d2a4c43a8c8b6a

Just check for C++ version.
This fixes build breakage introduced with r15736.

TBR=svenpanne@chromium.org

Review URL: https://codereview.chromium.org/19671009

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@15737 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/checks.h
@@ -232,9 +232,7 @@ inline void CheckNonEqualsHelper(const char* file,
 
 // Use C++11 static_assert if possible, which gives error
 // messages that are easier to understand on first sight.
-#if __cplusplus >= 201103L || \
-    (defined(__has_feature) && __has_feature(cxx_static_assert)) || \
-    (defined(__has_extension) && __has_extension(cxx_static_assert))
+#if __cplusplus >= 201103L
 #define STATIC_CHECK(test) static_assert(test, #test)
 #else
 // This is inspired by the static assertion facility in boost.  This