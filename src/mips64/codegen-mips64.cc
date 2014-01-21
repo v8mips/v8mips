ebbbe673694196df906ffd849f518cd55d3153e3
Yu Yin <xwafish@gmail.com>
2014-01-21 12:31:21 +0800

    MIPS64: Fix memory grow error when run mjsunit in debug mode.

--- src/mips64/codegen-mips64.cc
+++ src/mips64/codegen-mips64.cc
@@ -147,7 +147,6 @@ OS::MemCopyUint8Function CreateMemCopyUint8Function(
     // using kPrefHintPrepareForStore.
     ASSERT(pref_hint_store != kPrefHintPrepareForStore ||
            pref_chunk * 4 >= max_pref_size);
-     __ break_(0x221);
     // If the size is less than 8, go to lastb. Regardless of size,
     // copy dst pointer to v0 for the retuen value.
     __ slti(t2, a2, 2 * loadstore_chunk);
@@ -597,35 +596,34 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
   Label loop, entry, convert_hole, gc_required, only_change_map, done;
 
   Register scratch = t6;
- __ break_(0x222);
   if (mode == TRACK_ALLOCATION_SITE) {
     __ JumpIfJSArrayHasAllocationMemento(a2, t0, fail);
   }
 
   // Check for empty arrays, which only require a map transition and no changes
   // to the backing store.
-  __ lw(t0, FieldMemOperand(a2, JSObject::kElementsOffset));
+  __ ld(t0, FieldMemOperand(a2, JSObject::kElementsOffset));
   __ LoadRoot(at, Heap::kEmptyFixedArrayRootIndex);
   __ Branch(&only_change_map, eq, at, Operand(t0));
 
   __ push(ra);
-  __ lw(t1, FieldMemOperand(t0, FixedArray::kLengthOffset));
+  __ ld(t1, FieldMemOperand(t0, FixedArray::kLengthOffset));
   // t0: source FixedArray
   // t1: number of elements (smi-tagged)
 
   // Allocate new FixedDoubleArray.
-  __ sll(scratch, t1, 2);
-  __ Addu(scratch, scratch, FixedDoubleArray::kHeaderSize);
+  __ dsrl(scratch, t1, 32 - kDoubleSizeLog2);  // TODO(yy) macro-assemble.
+  __ Daddu(scratch, scratch, FixedDoubleArray::kHeaderSize);
   __ Allocate(scratch, t2, t3, t5, &gc_required, DOUBLE_ALIGNMENT);
   // t2: destination FixedDoubleArray, not tagged as heap object
 
   // Set destination FixedDoubleArray's length and map.
   __ LoadRoot(t5, Heap::kFixedDoubleArrayMapRootIndex);
-  __ sw(t1, MemOperand(t2, FixedDoubleArray::kLengthOffset));
-  __ sw(t5, MemOperand(t2, HeapObject::kMapOffset));
+  __ sd(t1, MemOperand(t2, FixedDoubleArray::kLengthOffset));
+  __ sd(t5, MemOperand(t2, HeapObject::kMapOffset));
   // Update receiver's map.
 
-  __ sw(a3, FieldMemOperand(a2, HeapObject::kMapOffset));
+  __ sd(a3, FieldMemOperand(a2, HeapObject::kMapOffset));
   __ RecordWriteField(a2,
                       HeapObject::kMapOffset,
                       a3,
@@ -635,8 +633,8 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
                       OMIT_REMEMBERED_SET,
                       OMIT_SMI_CHECK);
   // Replace receiver's backing store with newly created FixedDoubleArray.
-  __ Addu(a3, t2, Operand(kHeapObjectTag));
-  __ sw(a3, FieldMemOperand(a2, JSObject::kElementsOffset));
+  __ Daddu(a3, t2, Operand(kHeapObjectTag));
+  __ sd(a3, FieldMemOperand(a2, JSObject::kElementsOffset));
   __ RecordWriteField(a2,
                       JSObject::kElementsOffset,
                       a3,
@@ -648,10 +646,10 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
 
 
   // Prepare for conversion loop.
-  __ Addu(a3, t0, Operand(FixedArray::kHeaderSize - kHeapObjectTag));
-  __ Addu(t3, t2, Operand(FixedDoubleArray::kHeaderSize));
-  __ sll(t2, t1, 2);
-  __ Addu(t2, t2, t3);
+  __ Daddu(a3, t0, Operand(FixedArray::kHeaderSize - kHeapObjectTag));
+  __ Daddu(t3, t2, Operand(FixedDoubleArray::kHeaderSize));
+  __ dsll(t2, t1, 32 - kDoubleSizeLog2);  // TODO(yy) macro-assemble.
+  __ Daddu(t2, t2, t3);
   __ li(t0, Operand(kHoleNanLower32));
   __ li(t1, Operand(kHoleNanUpper32));
   // t0: kHoleNanLower32
@@ -662,7 +660,7 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
   __ Branch(&entry);
 
   __ bind(&only_change_map);
-  __ sw(a3, FieldMemOperand(a2, HeapObject::kMapOffset));
+  __ sd(a3, FieldMemOperand(a2, HeapObject::kMapOffset));
   __ RecordWriteField(a2,
                       HeapObject::kMapOffset,
                       a3,
@@ -680,16 +678,18 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
 
   // Convert and copy elements.
   __ bind(&loop);
-  __ lw(t5, MemOperand(a3));
-  __ Addu(a3, a3, kIntSize);
+  __ ld(t5, MemOperand(a3));
+  __ Daddu(a3, a3, kIntSize);
   // t5: current element
-  __ UntagAndJumpIfNotSmi(t5, t5, &convert_hole);
+  // __ UntagAndJumpIfNotSmi(t5, t5, &convert_hole);
+  __ JumpIfNotSmi(t5, &convert_hole);
+  __ SmiUntag(t5);
 
   // Normal smi, convert to double and store.
   __ mtc1(t5, f0);
   __ cvt_d_w(f0, f0);
   __ sdc1(f0, MemOperand(t3));
-  __ Addu(t3, t3, kDoubleSize);
+  __ Daddu(t3, t3, kDoubleSize);
 
   __ Branch(&entry);
 
@@ -697,14 +697,13 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
   __ bind(&convert_hole);
   if (FLAG_debug_code) {
     // Restore a "smi-untagged" heap object.
-    __ SmiTag(t5);
     __ Or(t5, t5, Operand(1));
     __ LoadRoot(at, Heap::kTheHoleValueRootIndex);
     __ Assert(eq, kObjectFoundInSmiOnlyArray, at, Operand(t5));
   }
   __ sw(t0, MemOperand(t3));  // mantissa
   __ sw(t1, MemOperand(t3, kIntSize));  // exponent
-  __ Addu(t3, t3, kDoubleSize);
+  __ Daddu(t3, t3, kDoubleSize);
 
   __ bind(&entry);
   __ Branch(&loop, lt, t3, Operand(t2));
@@ -725,7 +724,6 @@ void ElementsTransitionGenerator::GenerateDoubleToObject(
   //  -- t0    : scratch (elements)
   // -----------------------------------
   Label entry, loop, convert_hole, gc_required, only_change_map;
-__ break_(0x223);
   if (mode == TRACK_ALLOCATION_SITE) {
     __ JumpIfJSArrayHasAllocationMemento(a2, t0, fail);
   }
@@ -954,7 +952,6 @@ void MathExpGenerator::EmitMathExp(MacroAssembler* masm,
   ASSERT(ExternalReference::math_exp_constants(0).address() != NULL);
 
   Label zero, infinity, done;
- __ break_(0x225);
   __ li(temp3, Operand(ExternalReference::math_exp_constants(0)));
 
   __ ldc1(double_scratch1, ExpConstant(0, temp3));
