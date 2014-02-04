00c21a0c15cac8b32be310d370ea3d336bd27e62
Paul Lind <paul.lind@imgtec.com>
2014-02-03 22:33:53 -0800

    TODO: Builtins::Generate_OnStackReplacement() loads unaligned int64.
    Possibly create an unaligned ld macro ?

--- src/mips64/builtins-mips64.cc
+++ src/mips64/builtins-mips64.cc
@@ -987,6 +987,7 @@ void Builtins::Generate_OnStackReplacement(MacroAssembler* masm) {
 
   // Load deoptimization data from the code object.
   // <deopt_data> = <code>[#deoptimization_data_offset]
+// TODO(plind): this damn offset is unaligned -- make a Unaligned accessor? ..........
   __ ld(a1, MemOperand(v0, Code::kDeoptimizationDataOffset - kHeapObjectTag));
 
   // Load the OSR entrypoint offset from the deoptimization data.
