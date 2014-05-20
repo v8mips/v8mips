881262c3f0e9e2571e2e7243cebd777c30c6612d
Yu Yin <xwafish@gmail.com>
2014-05-20 16:53:12 +0800

    MIPS64: Fix push the formal parameter count argument to the generator function.
    TEST: mjsunit/harmony/generators-iteration

--- src/mips64/full-codegen-mips64.cc
+++ src/mips64/full-codegen-mips64.cc
@@ -2166,7 +2166,7 @@ void FullCodeGenerator::EmitGeneratorResume(Expression *generator,
   __ LoadRoot(a2, Heap::kTheHoleValueRootIndex);
   Label push_argument_holes, push_frame;
   __ bind(&push_argument_holes);
-  __ Dsubu(a3, a3, Operand(Smi::FromInt(1)));
+  __ Dsubu(a3, a3, Operand(1));
   __ Branch(&push_frame, lt, a3, Operand(zero_reg));
   __ push(a2);
   __ jmp(&push_argument_holes);
