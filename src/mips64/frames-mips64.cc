a61a2234b53d6af0634583bd86fbfa9ffe01842e
Akos Palfi <palfia@homejinni.com>
2014-05-27 23:27:10 +0200

    MIPS64: Fix some out-of-line constant pool garbage collection bugs.
    Port r18473 (5969bbb0)
    
    Original commit message:
    This CL fixes some bugs in the out of line constant pool implementation when
    constant pools are GCed.  Namely:
      - Push/Pop pp register in exit frames and VisitPointer on it to ensure it is
        updated if the ConstantPoolArray is moved by GC.
      - Mark pp as a SafePoint Register for optimized functions.
      - Ensure that StandardFrame::IterateExpressions also iterates over the
        constant pool pointer in the stackframe.
      - Fix calculation of last_ptr_offset in ConstantPoolArray body iterator.
      - Make ensure that CONSTANT_POOL_ARRAY_TYPE is a pointer object InstanceType.

--- src/mips64/frames-mips64.cc
+++ src/mips64/frames-mips64.cc
@@ -53,6 +53,13 @@ Register StubFailureTrampolineFrame::constant_pool_pointer_register() {
   return no_reg;
 }
 
+
+Object*& ExitFrame::constant_pool_slot() const {
+  UNREACHABLE();
+  return Memory::Object_at(NULL);
+}
+
+
 } }  // namespace v8::internal
 
 #endif  // V8_TARGET_ARCH_MIPS64
