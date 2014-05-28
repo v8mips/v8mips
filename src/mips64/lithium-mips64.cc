65de7e873536d1263a2e3add0f610035a8ad1cbb
Akos Palfi <palfia@homejinni.com>
2014-05-29 01:20:01 +0200

    MIPS64: Kill obsolete HLoadExternalArrayPointer instruction.
    Port r18893 (4cb88de9)

--- src/mips64/lithium-mips64.cc
+++ src/mips64/lithium-mips64.cc
@@ -1987,13 +1987,6 @@ LInstruction* LChunkBuilder::DoLoadRoot(HLoadRoot* instr) {
 }
 
 
-LInstruction* LChunkBuilder::DoLoadExternalArrayPointer(
-    HLoadExternalArrayPointer* instr) {
-  LOperand* input = UseRegisterAtStart(instr->value());
-  return DefineAsRegister(new(zone()) LLoadExternalArrayPointer(input));
-}
-
-
 LInstruction* LChunkBuilder::DoLoadKeyed(HLoadKeyed* instr) {
   ASSERT(instr->key()->representation().IsSmiOrInteger32());
   ElementsKind elements_kind = instr->elements_kind();
