83ab447e2ca2cd98144285609de0475a40989e17
Svetozar Janjic <svetozar.janjic@imgtec.com>
2014-05-30 18:47:54 +0200

    MIPS64: Fix build, use flag trace_sim_at.

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -587,6 +587,8 @@ DEFINE_bool(trace_parse, false, "trace parsing and preparsing")
 
 // simulator-arm.cc, simulator-a64.cc and simulator-mips.cc
 DEFINE_bool(trace_sim, false, "Trace simulator execution")
+DEFINE_int(trace_sim_at, 0,
+           "Simulator start tracing after x number of instructions")
 DEFINE_bool(debug_sim, false, "Enable debugging the simulator")
 DEFINE_bool(check_icache, false,
             "Check icache flushes in ARM and MIPS simulator")
