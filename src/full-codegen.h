2d1fd74366a1ce5adbaed7c551d63229bdfdbaf7
Akos Palfi <palfia@homejinni.com>
2014-06-05 00:35:49 +0200

    MIPS64: Adjust memory limits.
    Port r20825 (b1985245)

--- src/full-codegen.h
+++ src/full-codegen.h
@@ -139,6 +139,7 @@ class FullCodeGenerator: public AstVisitor {
   static const int kBootCodeSizeMultiplier = 120;
 #elif V8_TARGET_ARCH_MIPS64
   static const int kCodeSizeMultiplier = 142;
+  static const int kBootCodeSizeMultiplier = 120;
 #else
 #error Unsupported target architecture.
 #endif
