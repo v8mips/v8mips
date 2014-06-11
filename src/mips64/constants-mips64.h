4ff4e68241d973ac529187957639d46d73ff5d42
Akos Palfi <palfia@homejinni.com>
2014-06-12 01:02:52 +0200

    MIPS64: Documentation: fix comments for CommuteCondition.
    Port r21685 (929f3cf9)

--- src/mips64/constants-mips64.h
+++ src/mips64/constants-mips64.h
@@ -558,7 +558,7 @@ inline Condition NegateCondition(Condition cc) {
 }
 
 
-// Commute a condition such that a cond b == b cond' b.
+// Commute a condition such that {a cond b == b cond' a}.
 inline Condition CommuteCondition(Condition cc) {
   switch (cc) {
     case Uless:
