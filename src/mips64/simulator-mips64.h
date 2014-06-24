24d71ecf933ee2c55057b3ab36097b51290ca57c
Akos Palfi <palfia@homejinni.com>
2014-06-24 14:33:45 +0200

    MIPS64: Implement sim-stack-size flag.
    Some specific test requires larger sim stack size, like the mjsunit/harmony/proxies test.
    
    The sim-stack-size flag was passed to the test already, but it was ignored by the MIPS64 simulator.
    
    TEST=mjsunit/harmony/proxies

--- src/mips64/simulator-mips64.h
+++ src/mips64/simulator-mips64.h
@@ -400,7 +400,7 @@ class Simulator {
 
   // Simulator support.
   // Allocate 1MB for stack.
-  static const size_t stack_size_ = 2 * 1024*1024;
+  size_t stack_size_;
   char* stack_;
   bool pc_modified_;
   int64_t icount_;
